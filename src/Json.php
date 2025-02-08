2e38c7178d022c44514fa2d408a48f1df0832423
dackou <dackou@163.com>
2025-02-08 10:45:38 +0800

    first commit

--- src/Json.php
+++ src/Json.php
@@ -1,53 +1,59 @@
 <?php
 	namespace Dack;
 
+	use support\Response;
 	use Dack\Lang;
 
 	class Json{
 		public static function show($code = 1,$msg = ''){
 			if(!empty($msg)){
 				if(is_object($msg)){
-					return json_encode(['code' => $code, 'msg' => 'success', 'data' => $msg]);
+					return self::json(['code' => $code, 'msg' => 'success', 'data' => $msg]);
 				}
 				if(is_array($msg)){
 					if(!$msg){
-						return json_encode(['code' => $code,'msg' => 'success','data' => []]);
+						return self::json(['code' => $code,'msg' => 'success','data' => []]);
 					}
-					return json_encode(['code' => $code, 'msg' => 'success', 'data' => $msg]);
+					return self::json(['code' => $code, 'msg' => 'success', 'data' => $msg]);
 				}
-				return json_encode(['code' => $code, 'msg' => $msg]);
+				return self::json(['code' => $code, 'msg' => $msg]);
 			}else{
 				if(is_object($code)){
-					return json_encode(['code' => 0, 'msg' => 'success', 'data' => $code]);
+					return self::json(['code' => 0, 'msg' => 'success', 'data' => $code]);
 				}
 				if(is_array($code)){
 					if(!$code){
-						return json_encode(['code' => 0,'msg' => 'success','data' => []]);
+						return self::json(['code' => 0,'msg' => 'success','data' => []]);
 					}
 					if(isset($code['code'])){
-						return json_encode($code);
+						return self::json($code);
 					}
-					return json_encode(['code' => 0, 'msg' => 'success', 'data' => $code]);
+					return self::json(['code' => 0, 'msg' => 'success', 'data' => $code]);
 				}
 
 				if(is_bool($code)){
-					return json_encode($code ? ['code' => 0,'msg' => 'success'] : ['code' => 1,'msg' => 'fail']);
+					return self::json($code ? ['code' => 0,'msg' => 'success'] : ['code' => 1,'msg' => 'fail']);
 				}
 
 				if(is_numeric($code)){
-					return json_encode(['code' => $code,'msg' => Lang::get($code)]);
+					return self::json(['code' => $code,'msg' => Lang::get($code)]);
 				}
 
 				if(is_string($code)){
-					return json_encode(['code' => 1,'msg' => $code]);
+					return self::json(['code' => 1,'msg' => $code]);
 				}
 
 				if(is_null($code) || empty($code) || $code === 0){
-					return json_encode(['code' => 0,'msg' => 'success']);
+					return self::json(['code' => 0,'msg' => 'success']);
 				}
 
-				return json_encode(['code' => 1,'msg' => $code]);
+				return self::json(['code' => 1,'msg' => $code]);
 			}
+		}
+
+		private static function json($data, $options = JSON_UNESCAPED_UNICODE)
+		{
+		    return new Response(200, ['Content-Type' => 'application/json'], json_encode($data, $options));
 		}
 	}
 ?>
